e037246196ea7eba02b2bc85d5e390ef4cbcb132
jaimie-mmhs <jaimie@mmhsbulldogs.org>
2025-10-09 16:03:12 -0400

    added menus

--- src/site.config.ts
+++ src/site.config.ts
@@ -6,14 +6,33 @@ export const site = {
   email: 'info@mmhsbulldogs.org',
   nav: [
     { label: 'Home', href: '/' },
-    { label: 'Academics', href: '/academics' },
-    { label: 'About', href: '/about' },
-    { label: 'Staff', href: '/about/staff' },
+    { label: 'Academics', href: '/academics', children: [
+      { label: 'Library', href: '/academics/library' },
+      { label: 'CTE', href: '/academics/cte' },
+      { label: 'Special Education', href: '/about/special-education,' },
+      { label: 'JMG', href: '/academics/jmg' },
+      { label: 'Guidance', href: '/academics/guidance' }
+    ] },
+    { label: 'About', href: '/about', children: [
+      { label: 'Mission', href: '/about/mission' },
+      { label: 'History', href: '/about/history' },
+      { label: 'Staff', href: '/about/staff' },
+      { label: 'Bulldog Gear', href: '/about/gear' },
+      { label: 'Map', href: '/about/map' }
+    ]},
+    
     { label: 'Calendar', href: '/calendar' },
     { label: 'Meals', href: '/meals' },
-    { label: 'Activities', href: '/activities' },
+    { label: 'Activities', href: '/activities', children: [
+      { label: 'Clubs', href: '/activities/clubs' },
+      { label: 'Athletics', href: '/activities/athletics' }
+    ] },
     { label: 'Covid-19', href: '/covid' },
-    { label: 'Bulldog Gear', href: '/gear' },
+    { label: 'Forms', href: '/forms', children: [
+      { label: 'Change of Address', href: 'https://forms.gle/DttSs3cXXvytjUNi9' },
+      { label: 'SAT Saturday Registration', href: 'https://www.collegeboard.org/' },
+      { label: 'Student Emergency Form', href: 'https://forms.gle/ws3bMPCUqmBwjekTA' }
+    ] },
     { label: 'Technology', href: '/tech'}
   ]
 };
