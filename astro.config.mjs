ba96ca191aca17c3203f9fdbf54f61ab506001fa
jaimie-mmhs <jaimie@mmhsbulldogs.org>
2025-10-09 15:01:52 -0400

    Fixed file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://mmhsbulldogs.org',
   integrations: [tailwind({ applyBaseStyles: true })],
-  vite: { server: { port: 4321 } }
+  vite: { 
+    server: { port: 4321 },
+    resolve: { alias: { '@': '/src', '~': '/src' } }
+  }
+
 });
