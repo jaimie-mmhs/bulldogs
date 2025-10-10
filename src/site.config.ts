95eacbaef05b3145268b1b79287c0fef24156842
jaimie-mmhs <jaimie@mmhsbulldogs.org>
2025-10-10 14:23:54 -0400

    modifying menus

--- src/site.config.ts
+++ src/site.config.ts
@@ -10,43 +10,75 @@ export const site = {
   url: 'https://test.mmhsbulldogs.org', //<--comment out when site goes live
   //url: 'https://www.mmhsbulldogs.org',  //<-- uncomment when site goes live
   nav: [
-    { label: 'Home', href: '/' },
-    { label: 'Academics', href: '/academics', children: [
+      { label: 'Home', href: '/' },
+
+  {
+    label: 'Academics',
+    href: '/academics',
+    children: [
       { label: 'Library', href: '/academics/library' },
       { label: 'CTE', href: '/academics/cte' },
       { label: 'Special Education', href: 'https://www.aos96.org/special-education-services' },
       { label: 'JMG', href: '/academics/jmg' },
       { label: 'Guidance', href: '/academics/guidance' }
-    ] },
-    { label: 'About', href: '/about', children: [
+    ]
+  },
+
+  {
+    label: 'About',
+    href: '/about',
+    children: [
       { label: 'Mission', href: '/about/mission' },
       { label: 'History', href: '/about/history' },
       { label: 'Staff', href: '/about/staff' },
       { label: 'Bulldog Gear', href: '/about/gear' },
       { label: 'Map', href: '/about/map' }
-    ]},
-    
-    { label: 'Calendar', href: '/calendar' },
-    { label: 'Meals', href: '/meals' },
-    { label: 'Activities', href: '/activities', children: [
+    ]
+  },
+
+  { label: 'Calendar', href: '/calendar' },
+  { label: 'Meals', href: '/meals' },
+
+  {
+    label: 'Activities',
+    href: '/activities',
+    children: [
       { label: 'Clubs', href: '/activities/clubs' },
       { label: 'Athletics', href: '/activities/athletics' }
-    ] },
-    { label: 'Covid-19', href: '/covid' },
-    { label: 'Forms', href: '/forms', children: [
+    ]
+  },
+
+  { label: 'Covid-19', href: '/covid' },
+
+  {
+    label: 'Forms',
+    href: '/forms',
+    children: [
       { label: 'Change of Address', href: 'https://forms.gle/DttSs3cXXvytjUNi9' },
       { label: 'SAT Saturday Registration', href: 'https://www.collegeboard.org/' },
-      { label: 'Student Emergency Form', href: 'https://forms.gle/ws3bMPCUqmBwjekTA' }
-    ] },
-    { label: 'Seniors', children: [
-      {label: 'College Release', href: 'https://forms.gle/j55rYXPWnPUq5GEd6'},
-      {label: 'FAFSA', href: 'https://studentaid.gov/h/apply-for-aid/fafsa'},
-      {label: 'FSAID', href: 'https://www.famemaine.com/affording-education/pay-for-school/get-financial-aid/fafsa/'}
-    ]},
-      { label: 'Graduates', children: [
-      {label: 'Scholarship Redemption', href: 'https://forms.gle/i16pGtjnTtj8TYgS9'},
-      {label: 'Transcript Request', href: 'https://forms.gle/5hTddfqMcwZ4TK1t9'}
-    ]},
-    { label: 'Technology', href: '/tech'}
+      { label: 'Student Emergency Form', href: 'https://forms.gle/ws3bMPCUqmBwjekTA' },
+
+      // Group: Seniors (no href on the group)
+      {
+        label: 'Seniors',
+        children: [
+          { label: 'College Release', href: 'https://forms.gle/j55rYXPWnPUq5GEd6' },
+          { label: 'FAFSA', href: 'https://studentaid.gov/h/apply-for-aid/fafsa' },
+          { label: 'FSAID', href: 'https://www.famemaine.com/affording-education/pay-for-school/get-financial-aid/fafsa/' }
+        ]
+      },
+
+      // Group: Graduates
+      {
+        label: 'Graduates',
+        children: [
+          { label: 'Scholarship Redemption', href: 'https://forms.gle/i16pGtjnTtj8TYgS9' },
+          { label: 'Transcript Request', href: 'https://forms.gle/5hTddfqMcwZ4TK1t9' }
+        ]
+      }
+    ]
+  },
+
+  { label: 'Technology', href: '/tech' }
   ]
 };
